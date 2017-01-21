577f57318a57e42bebdcdb27a241f73731f54a79
mpnunez <mpnunez@udel.edu>
2017-01-21 17:34:11 -0500

    Made STS KMC with SA

--- Cpp_port/network.cpp
+++ Cpp_port/network.cpp
@@ -1,5 +1,5 @@
-#include <iostream>    // using IO functions
-#include <string>      // using string
+# include <iostream>    // using IO functions
+# include <string>      // using string
 # include <vector>
 # include <fstream>
 using namespace std;
@@ -9,15 +9,15 @@ class rxn_network {
 private:
 
 	// Specify the reaction system - used for all models
-	//string spec_names[5];                 // names of chemical species
-	int N_0[5];                        // initial state
-	//int stoich[][];                     // stoichiometry matrix
-	//int stoich_react[][];               // stoichiometric matrix for reactants only
-	double k[5];                          // rate constants of elementary reactions
-	//string param_names[5];                // names of the parameters
-	double t_final;                    // termination time (s)
-	int N_record;            // number of time points to record
-	int fast_rxns[5];                  // indices of the fast reactions
+	string spec_names[5];                   // names of chemical species
+	int N_0[5];                             // initial state
+	//int stoich[][];                       // stoichiometry matrix
+	//int stoich_react[][];                 // stoichiometric matrix for reactants only
+	double k[5];                            // rate constants of elementary reactions
+	string param_names[5];                  // names of the parameters
+	double t_final;                         // termination time (s)
+	int N_record;                           // number of time points to record
+	int fast_rxns[5];                       // indices of the fast reactions
 	
 	// Only for STS ODE and KMC
 	double eps;                	// stiffness level, eps << 1
@@ -34,6 +34,48 @@ public:
 	
 	int get_N_record(){
 		return N_record;
+	}
+    
+    void read_input_file(){
+        
+		//string fname = "input.txt";
+        
+        char letter;
+        ifstream reader("input.txt");
+        
+        
+        
+        if(! reader){
+            
+            cout << "Error opening file." << endl;
+        
+        }   else{
+            
+            for(int i=0; ! reader.eof(); i++){
+                
+                reader.get(letter);
+                cout << letter;
+            }
+            
+            cout << endl;
+            reader.close();
+        }
+    }
+    
+    void write_output_file(){    
+        
+        ofstream writer("output.txt");
+        
+        if(! writer){
+            
+            cout << "Error opening file" << endl;
+
+            
+        }   else {
+            
+            writer << "This is the output." << endl;
+            writer.close();
+        }
 	}
 
 /*
@@ -54,5 +96,8 @@ int main() {
 	
 	cout << rn.get_N_record() << endl;
 	
+    rn.read_input_file();
+    rn.write_output_file();
+    
 	return 0;
 }
