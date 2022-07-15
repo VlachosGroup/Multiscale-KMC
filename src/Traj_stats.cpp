11e0fa23b5bb13a76997ae282666389a28eea227
Marcel Nunez <mpnunez28@gmail.com>
2022-07-14 21:22:22 -0700

    Revert "Make input file match in cpp file and example"
    This reverts commit 6f35dbc8ded2e49c2b1da5dca03d51ca1664163d.

--- src/Traj_stats.cpp
+++ src/Traj_stats.cpp
@@ -207,7 +207,7 @@ int main() {
     //    cout << "x is NaN" << endl;
     //}
     cout.precision(6);
-	file_reader fr("network.in");               // Read input file
+	file_reader fr("MSA_in.txt");               // Read input file
     Traj_stats sim; 
     sim.in_data = fr;                   // assign input
     sim.run_simulations();              // run KMC simulations, will execute differently depending on fr.two_time_scale
